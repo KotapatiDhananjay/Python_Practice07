944825b7393153598734ab314286850954d481a1
Kotapati Dhananjay <your-email@example.com>
2026-07-27 17:14:19 +0530

    Initial commit

---
+++ task 1.py
@@ -0,0 +1,10 @@
+import psycopg2
+conn = psycopg2.connect(dbname="postgres", user="postgres", password="Dhananjay@05", host="Localhost", port="5432")
+
+cursor = conn.cursor()
+cursor.execute('''create table employees(Name Text, ID int, Age int);''')
+print("Table created successfully")
+
+conn.commit()
+conn.close()
+
